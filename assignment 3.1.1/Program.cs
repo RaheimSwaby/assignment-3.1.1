0709126fc90f033ce6a4a08147904d21e90b55f1
Rahei <Rahei@RAHEIMSPC>
2024-11-18 11:27:31 -0500

    Add project files.

---
+++ assignment 3.1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace assignment_3._1._1
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(ReturnByTwosEvens(100));
+            }
+
+            static string ReturnByTwosEvens(int numAmount)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 2; i < numAmount; i += 2)
+                {
+                    sb.Append(i);
+                    sb.Append(' ');
+                }
+                return sb.ToString();
+            }
+
+        }
+    }
+
+   
